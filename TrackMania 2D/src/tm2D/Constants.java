45f4878cae5336f6c4533b2757fba9cce88e70fe
mikrelef <mathieu.ikrelef@devoteam.com>
2019-03-22 15:03:00 +0100

    is unix for path

--- TrackMania 2D/src/tm2D/Constants.java
+++ TrackMania 2D/src/tm2D/Constants.java
@@ -3,7 +3,7 @@ package tm2D;
 import java.io.File;
 
 public interface Constants {
-
-  String RELATIVE_PATH = "TrackMania 2D" + File.separatorChar;
+  String OS = System.getProperty("os.name").toLowerCase();
+  String RELATIVE_PATH = (OS.contains("nix") || OS.contains("nux") || OS.contains("aix")) ? "TrackMania 2D" + File.separatorChar : "";
   String RELATIVE_PATH_IMAGE_CIRCUIT = RELATIVE_PATH + "ImagesCircuit" + File.separatorChar;
 }
